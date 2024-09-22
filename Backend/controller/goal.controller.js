ec003f5fa98c9d688a2088831d4a801012e0aabe
masterha <masterha@sheridancollege.ca>
2024-09-22 17:56:37 -0400

    setGoal method

--- Backend/controller/goal.controller.js
+++ Backend/controller/goal.controller.js
@@ -1,3 +1,47 @@
+const { where } = require("sequelize");
+const db = require("../models"); // Assuming db is the Sequelize instance
+const Goals = db.goals;
+
+
 exports.test = async (req, res)=>{
-    
+}
+
+exports.getGoal = async (req, res) =>{
+
+}
+
+exports.setGoal = async (req, res) =>{
+    const {user_id, protein, calories, sugar } = req.body;
+
+    if (!user_id || !protein || !calories || !sugar){
+        return res.status(500).send("Invalid data");
+    }
+
+    try{
+        const checkGoal = await Goals.findOne({
+            where: {
+                userId: user_id
+            }
+        })
+
+        if (checkGoal){
+            return res.status(500).send({message: "user already has a goal", goal: checkGoal})
+        }
+        const setGoal = await Goals.create({
+            userId: user_id,
+            protein: protein,
+            calories: calories,
+            sugar: sugar
+        });
+
+        if (setGoal){
+            return res.status(200).send({goal: setGoal});
+        }
+
+        return res.status(400).send({error: "Goal not set"});
+
+    }catch (err){
+        return res.status(500).send("Error setting goal");
+    }
+
 }
