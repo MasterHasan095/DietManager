365a26020304ceed1475b637533a81cbe727666c
masterha <masterha@sheridancollege.ca>
2024-09-22 17:56:01 -0400

    changed authenticateToken location

--- Backend/routes/goals.routes.js
+++ Backend/routes/goals.routes.js
@@ -1,6 +1,12 @@
 const {goal} = require("../models/goal.model.js");
 const controller = require("../controller/goal.controller.js");
+const {authenticateToken} = require("../middleware/auth.js")
 
 module.exports = function (app){
     app.post("/goalTest", controller.test);
+
+    app.post("/setGoal",authenticateToken, controller.setGoal);
+    app.get("/getGoal",authenticateToken, controller.getGoal);
+
 }
+
